a1194b761949234289d27fab270744129e2ecec3
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2017-04-30 23:54:20 -0400

    Updated the python predictor

--- src/predictor.py
+++ src/predictor.py
@@ -7,6 +7,7 @@ import scipy
 from sklearn.externals import joblib
 import os
 import argparse
+import sys
 
 # # Function convert test vector to desirable formate
 
@@ -28,42 +29,52 @@ def get_prediction(test_data, model, class_name):
     return pred_label, pred_prob
 
 
-# # Load test data
+if __name__ == "__main__":
 
-parser = argparse.ArgumentParser()
-parser.add_argument("testData", help="enter a test vector for the prediction model")
-test_arg = parser.parse_args()
-test_data = np.asarray([x.strip() for x in  test_arg.testData.split(',')])
-# print(test_data)
+    # # Load test data
 
-
-# # Import Feature names and Class names
-
-model_name = np.load(os.path.join(os.getcwd() ,'../model/feature_class_name.npz'))
-feature_name = model_name['arr_0']
-class_name = model_name['arr_1']
+    parser = argparse.ArgumentParser()
+    parser.add_argument("testData", help="enter a test vector for the prediction model")
+    test_arg = parser.parse_args()
+    test_data = np.asarray([x.strip() for x in  test_arg.testData.split(',')])
+    # print(test_data)
 
 
-# # Load the trained Linear SVM models
+    # # Import Feature names and Class names
+
+    model_name = np.load(os.path.join(os.getcwd() ,'garuda_python/model/feature_class_name.npz'))
+    feature_name = model_name['arr_0']
+    class_name = model_name['arr_1']
 
 
-# classfier without weighting
-clf = joblib.load(os.path.join(os.getcwd() ,'../model/classifier_no_weighting.pkl'))
-# classfier class weighting
-wclf = joblib.load(os.path.join(os.getcwd() ,'../model/classifier_class_weighting.pkl'))
-# classfier sample weighting
-wsclf = joblib.load(os.path.join(os.getcwd() ,'../model/classifier_sample_weighting.pkl'))
+    # # Load the trained Linear SVM models
 
 
-# # Testing 
+    # classfier without weighting
+    clf = joblib.load(os.path.join(os.getcwd() ,'garuda_python/model/classifier_no_weighting.pkl'))
+    # classfier class weighting
+    wclf = joblib.load(os.path.join(os.getcwd() ,'garuda_python/model/classifier_class_weighting.pkl'))
+    # classfier sample weighting
+    wsclf = joblib.load(os.path.join(os.getcwd() ,'garuda_python/model/classifier_sample_weighting.pkl'))
 
-test_in = format_test_vector(test_data, feature_name)
-pl, pp = get_prediction(test_in, clf, class_name)
-print(pl[0], pp[0]*100)
-pl, pp = get_prediction(test_in, wclf, class_name)
-print(pl[0], pp[0]*100)
-pl, pp = get_prediction(test_in, wsclf, class_name)
-print(pl[0], pp[0]*100)
+
+    # # Testing 
+
+    test_in = format_test_vector(test_data, feature_name)
+    
+    println("Classifier without weighting")
+    pl, pp = get_prediction(test_in, clf, class_name)
+    println(pl[0], pp[0]*100)
+    
+    println("Classifier with weighting")
+    pl, pp = get_prediction(test_in, wclf, class_name)
+    println(pl[0], pp[0]*100)
+
+    println("Classifier with sample weighting")
+    pl, pp = get_prediction(test_in, wsclf, class_name)
+    println(pl[0], pp[0]*100)
+
+    sys.exit(0)
 
 
 
